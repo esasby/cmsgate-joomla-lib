c3f46e8dc1c9f233dba152d79554feab61ba6e68
n.mekh <ecvr813b>
2020-12-23 12:30:40 +0300

    HGCG-75: методы для корректного удаления плагина

--- src/esas/cmsgate/joomla/InstallHelperJoomla.php
+++ src/esas/cmsgate/joomla/InstallHelperJoomla.php
@@ -4,9 +4,28 @@
 namespace esas\cmsgate\joomla;
 
 
+use JFile;
+use JFolder;
+
 class InstallHelperJoomla
 {
     public static function dbActivateExtension(){
         CmsgateModelJoomla::activateExtension();
     }
+
+    public static function deleteWithLogging($file)
+    {
+        $deleted = true;
+        if (is_dir($file)) {
+            JFolder::delete($file);
+            $deleted = !JFolder::exists($file);
+        } elseif (is_file($file)) {
+            $deleted = JFile::delete($file);
+        }
+        if (!$deleted) {
+            echo JText::sprintf('JLIB_INSTALLER_ERROR_FILE_FOLDER', $file) . '<br />';;
+        }
+        return $result;
+    }
+
 }
